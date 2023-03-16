8f5da956d63f31b0c8932d8be7a451210d156933
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-03-16 16:50:43 -0400

    Added comments to ask for guidance while adding comments to isolate what needs work in js script

--- Assets/script.js
+++ Assets/script.js
@@ -1,59 +1,124 @@
-const quizContainer = document.getElementById('quizContainer')
 
+const quizBank = document.getElementById("questionBank");
+const quizContainer = document.getElementById("quizContainer");
+const startButt = document.getElementById("startQuizBtn");
+const resultsBox = document.getElementById("resultsContainer");
+const resultBtn = document.getElementById("vieResultsBtn");
+const timer = document.getElementById("timer");
+const error = document.getElementById("errorMessage");
+
+// make more later for each html id-- update should be all but never know
+// All this is correct
 let quizQuestions = [
-    {
-        question: 'What is JavaScript primarily used for?',
-        options:['Creating dynamic websites', 'Designing user interfaces', 'Building databases', 'Writing server-side code'],
-        answer: 'Creating dynamic websites',
-    },
-    {
-        question: 'Which of the following is a JavaScript data type?',
-        options:['Integer', 'Float', 'Boolean', 'Character'],
-        answer: 'Boolean',
-    },
-    {
-        question: 'What is the result of the following expression: "2" + 2?',
-        options:['"4"', '"22"', '4', '0',],
-        answer: '"22"',
-    },
-    {
-        question: 'What does the keyword "this" refer to in JavaScript?',
-        options:['The current function', 'The global object', 'The object that the function is a method of', 'The object that the function was called on',],
-        answer: 'The object that the function was called on',
-    },
-    {
-        question: 'WWhich of the following statements is true about JavaScript functions?',
-        options:['They must always return a value', 'They can only be declared using the "function" keyword', 'They can be passed as arguments to other functions', 'They cannot access variables declared outside of the function',],
-        answer: 'They can be passed as arguments to other functions',
-    },
-    {
-        question: 'Which of the following methods can be used to add an element to the end of an array?',
-        options:['push()', 'unshift()', 'pop()', 'shift()',],
-        answer: 'push()',
-    }
-]
+  {
+    question: "What is JavaScript primarily used for?",
+    options: [
+      "Creating dynamic websites",
+      "Designing user interfaces",
+      "Building databases",
+      "Writing server-side code",
+    ],
+    answer: "Creating dynamic websites",
+  },
+  {
+    question: "Which of the following is a JavaScript data type?",
+    options: ["Integer", "Float", "Boolean", "Character"],
+    answer: "Boolean",
+  },
+  {
+    question: 'What is the result of the following expression: "2" + 2?',
+    options: ['"4"', '"22"', "4", "0"],
+    answer: '"22"',
+  },
+  {
+    question: 'What does the keyword "this" refer to in JavaScript?',
+    options: [
+      "The current function",
+      "The global object",
+      "The object that the function is a method of",
+      "The object that the function was called on",
+    ],
+    answer: "The object that the function was called on",
+  },
+  {
+    question:
+      "Which of the following statements is true about JavaScript functions?",
+    options: [
+      "They must always return a value",
+      'They can only be declared using the "function" keyword',
+      "They can be passed as arguments to other functions",
+      "They cannot access variables declared outside of the function",
+    ],
+    answer: "They can be passed as arguments to other functions",
+  },
+  {
+    question:
+      "Which of the following methods can be used to add an element to the end of an array?",
+    options: ["push()", "unshift()", "pop()", "shift()"],
+    answer: "push()",
+  },
+];
+console.log(quizQuestions);
+// Seems correct
+quizQuestions.forEach(function (question, index) {
+  const questionElem = document.createElement("h2");
+  questionElem.textContent =
+    "Question " + (index + 1) + " : " + question.question;
+
+  const optionsElem = document.createElement("div");
+  optionsElem.classList.add("options");
+
+  question.options.forEach(function (option) {
+    const optionElem = document.createElement("button");
+    optionElem.textContent = option;
+    optionsElem.appendChild(optionElem);
+  });
+  quizContainer.appendChild(questionElem);
+  quizContainer.appendChild(optionsElem);
+});
+
+// function getElementByQuestion() {
+//   let question = questionElem((question.textContent = questionElem));
+// }
+
+startButt.addEventListener("click", startQuiz);
+//this function should do the following:
+//Or this function should do some of the following
+//  On start quiz i want startQuizBtn to display as none
+//  Then I want question 1 & a timer to display
+//  On clicking an answer, if correct next question, if 
+//incoreect, time is subtracted from total, and user is left to answer again
+//  if time finishes before all answers, error message 
+//saying "time is up" will display and move to score screen
+//on last question display submit button
+//if finish all before time, then end message, then hide submit button, leave time displaying end time.  
+//Have box to enter highscore, and new start button
+
+function startQuiz() {
+  let startScreenID = document.getElementById("quizBlock");
+  startScreenID.setAttribute("class", "showNone");
+  quizBank.removeAttribute("class");
+
+//above should be correct for thew most part //
+  // ask someone smarter for help my man
 
 
-
-/*  Consider below when you start building the timer, this example is for a timer that ends after the selected items are complete or after 15 minutes maybe itll help.    
-Ref below:
-            https://codepen.io/adammatz/pen/bwGqKr
-
-
-function delayedAlert() {
-    timeoutID = window.setTimeout(slowAlert, 1000);
-    }
-  
-  function slowAlert() {
-      var x;
-      if (alert("Your time is up! Please press okay to submit your quiz into the administrator!") == true) {
-          x = alert("");
-      } else {
-          x = alert("Please go to the front desk and wait for further instructions. Thank you!");
-      }
-      document.getElementById("demo").innerHTML = x;
+  //when press start squiz: Timer starts countdown
+  function timerOn() {
+    let secsLeft = (document.getElementById("timer").innerHTML = seconds + "s");
+    timer.setInterval(() => {
+      Attribute("class", "showNone");
+    }, interval);
   }
-  
+  if (timeleft > 0) {
+    clearInterval();
+    document.getElementById(secsLeft).innerHTML = "";
+  } else {
+    document.getElementById(errorMessage).innerHTML = "Time is up!";
+  }
+  //ref start time in notes
 
-  this ref could probably be all you need :
-        https://codepen.io/adammatz/pen/bwGqKr */
+  getElementByQuestion();
+}
+console.log(startQuiz);
+console.log(quizContainer);
--- README.md
+++ README.md
@@ -1,33 +1,36 @@
 # Quick-InQueries-Quiz To-do
-### ==================================================================================
+### ==============================================================================
 
+### Done
+*   Add questions and answers: array and added into html
+*   css hover for answers?: change background to be     darker or somethin
+### in progress
+*   make button start test on click
+*   styling can be minimal but have fun
 
-    Add questions and answers: array and added into html
-    make button start test on click
-    add timer, have time per question or per quiz? both? 
-    styling can be minimal but have fun 
-    have a way to calculate score
-    css hover for answers?: change background to be darker or somethin
-    maybe have a light mode dark mode if you have time
-### ==================================================================================
+### Needs Done
+*   add timer, have time per question or per quiz? both? 
+*   have a way to calculate score
+*  maybe have a light/dark mode if you have time
+### ==============================================================================
 
-##  To satisfy the user story and acceptance criteria, the quiz interface would need to have the following styles:
+##  To satisfy the user story and acceptance criteria, the quiz interface would need :
 
 ### Overall Layout
-    The quiz interface should be centered on the page and have appropriate margins and padding.
-    The interface should have a clean, modern design that is easy to read and navigate.
+    *   The quiz interface should be centered on the page and have appropriate margins and padding.
+    *   The interface should have a clean, modern design that is easy to read and navigate.
 ### Buttons
-    The Start Quiz, Submit Answers, and View Results buttons should be styled with appropriate colors and borders.
-    Hover and active states should be clearly indicated so users know when they are interacting with the buttons.
+    * The Start Quiz, Submit Answers, and View Results buttons should be styled with appropriate colors and borders.
+    * Hover and active states should be clearly indicated so users know when they are interacting with the buttons.
 ### Timer
-    The timer should be prominently displayed and easily readable.
-    When the timer reaches 10 seconds or less, it should turn red to indicate the end of the quiz is near.
+    *   The timer should be prominently displayed and easily readable.
+    *   When the timer reaches 10 seconds or less, it should turn red to indicate the end of the quiz is near.
 ### Question and Answer Options
-    Each question should be clearly labeled and have appropriate spacing and margins.
-    The answer options should be clearly labeled and styled to make it easy for users to select their answer.
-    The correct answer should be clearly indicated after the user selects an answer.
+    *   Each question should be clearly labeled and have appropriate spacing and margins.
+    *   The answer options should be clearly labeled and styled to make it easy for users to select their answer.
+    *   The correct answer should be clearly indicated after the user selects an answer.
 ### Game Over Screen
-    After the quiz is over, the user should be presented with a screen that allows them to enter their initials and see their score.
-    The screen should have a clean and modern design that is easy to read and navigate.
+    *   After the quiz is over, the user should be presented with a screen that allows them to enter their initials and see their score.
+    *   The screen should have a clean and modern design that is easy to read and navigate.
 
 ### ==================================================================================
